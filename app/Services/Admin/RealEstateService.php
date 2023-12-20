781fb644822cac5b74abb4ba97fcdc8f884c3712
MostafaMahmoud97 <mostafamahmoud111115@gmail.com>
2023-12-20 18:50:01 +0200

    edit in real estate service

--- app/Services/Admin/RealEstateService.php
+++ app/Services/Admin/RealEstateService.php
@@ -234,7 +234,7 @@ class RealEstateService
 
         if ($request->property_usage_id && $request->property_usage_id != null && $request->property_usage_id != "" ){
             $Units = $Units->whereHas("RealEstate",function ($q) use ($request){
-                $q->whereIn("building_type_use_id",$request->property_usage_id);
+                $q->where("building_type_use_id",$request->property_usage_id);
             });
         }
 
